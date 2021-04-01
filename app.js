191d09b626b86ec017b73fa1c7fbfb05445018a3
cleverGek <kg.geku@gmail.com>
2021-04-01 07:41:51 +0100

    url page

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var autreRouter = require('./routes/autres');
 var contactRouter =  require('./routes/contact');
+var urlRouter = require('./routes/url');
 
 var app = express();
 
@@ -35,6 +36,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/', autreRouter);
 app.use('/contact', contactRouter);
+app.use('/url', urlRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/url.js
+++ routes/url.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET url page */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ykola' });
+  res.render('url', { title: 'ykola' });
 });
 
 module.exports = router;
