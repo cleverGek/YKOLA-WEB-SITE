97448129b6dad19662e3744aa93e95bc51330548
cleverGek <kg.geku@gmail.com>
2021-04-02 18:00:46 +0100

    les add promandats

--- models/ProMandats.js
+++ models/ProMandats.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const ProMandats = mongoose.Schema({
-    _id: mongoose.Types.ObjectId,
     auteur: {type: String, required: true},
     promesse: {type: String, required: true},
     contenu: {type: String, required: true},
@@ -9,10 +8,6 @@ const ProMandats = mongoose.Schema({
     lieu: {type: String, required: true},
     circonstance: {type: String, required: true},
     tauxProgress: {type: String, required: true},
-    flag: {type: Boolean, default: true},
-    deleted: {type: Boolean, default: false}
-},{
-    timestamps : true
 });
 
 module.exports = mongoose.model('ProMandats', ProMandats);
