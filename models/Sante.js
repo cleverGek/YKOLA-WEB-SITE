acbf5977e97fdd3634ef084656e8b5e85eddd1a3
cleverGek <kg.geku@gmail.com>
2021-04-02 18:29:35 +0100

    Sport

--- models/Sante.js
+++ models/Sante.js
@@ -1,18 +1,11 @@
 const mongoose = require('mongoose');
 
 const Tendances = mongoose.Schema({
-    _id: mongoose.Types.ObjectId,
-    category: {type: Number, required: true},
     titre: {type: Number, required: true},
     image: {type: String, required: true},
-    titre: {type: String, required: true},
     vrai: {type: String, required: true},
     faux: {type: String, required: true},
-    sommaire: {type: Number, required: true},
-    flag: {type: Boolean, default: true},
-    deleted: {type: Boolean, default: false}
-},{
-    timestamps : true
+    sommaire: {type: Number, required: true}
 });
 
 module.exports = mongoose.model('Tendances', Tendances);
