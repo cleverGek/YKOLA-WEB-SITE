8624cafb611113ec4bf857a9a5f07afbf9fb403e
cleverGek <kg.geku@gmail.com>
2021-04-03 11:37:00 +0100

    la Sante

---
+++ models/Sport.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const Tendances = mongoose.Schema({
+    titre: {type: Number, required: true},
+    image: {type: String, required: true},
+    vrai: {type: String, required: true},
+    faux: {type: String, required: true},
+    sommaire: {type: Number, required: true}
+});
+
+module.exports = mongoose.model('Tendances', Tendances);
