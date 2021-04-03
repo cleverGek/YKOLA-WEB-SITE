16f7645b4340926847c86c6ef4d8d289952276c8
cleverGek <kg.geku@gmail.com>
2021-04-03 20:23:00 +0100

    je vais le faire

--- routes/ressources.js
+++ routes/ressources.js
@@ -15,11 +15,18 @@ router.get('/videos', function(req, res, next) {
 
 /* GET find all DOC. */
 router.get('/documents', async function(req, res, next) {
-
   const documents = await Documents.find().sort({
     id: 'desc'
   })
   res.render('showDOC', { documents: documents});
+});
+
+/* GET find by id. */
+router.get('/documents/:id', async (req, res, next) => {
+  const document = await Documents.findOne({ id: req.params.id});
+  if (document == null) res.redirect('/ressources/documents')
+    res.render('detailDOC', {document: document});
+  
 });
 
 module.exports = router;
