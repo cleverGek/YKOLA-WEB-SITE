37e1be7fc655d5c72d6dd1ebed8e50942ed4dad8
cleverGek <kg.geku@gmail.com>
2021-04-06 15:02:26 +0100

    image, multer pour partie promandats

--- routes/admPromandats.js
+++ routes/admPromandats.js
@@ -1,6 +1,29 @@
 var express = require('express');
 var ProMandats = require('../models/ProMandats');
 var router = express.Router();
+var multer =  require('multer');
+
+//define storage for image
+const storage = multer.diskStorage({
+
+    //destionnation
+    destination:function(req, file, collback){
+        collback(null, './public/ressources/images');
+    },
+    //add extension
+    filename:function(req, file, collback){
+       collback(null, Date.now()+file.originalname);
+    }
+})
+
+//upload parametres multer
+
+const upload = multer({
+    storage:storage,
+    limits:{
+        fieldSize:1024*1024*3
+    }
+});
 
 /* GET a amgin ressources pages */
 router.get('/addPROMANDATS', function(req, res, next) {
