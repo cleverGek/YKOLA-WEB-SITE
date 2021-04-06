459d60f36611073ad9ed65445b0a5f22923e24b1
cleverGek <kg.geku@gmail.com>
2021-04-06 22:24:12 +0100

    upload promandats

--- routes/proMandats.js
+++ routes/proMandats.js
@@ -3,8 +3,22 @@ var router = express.Router();
 var Promandats = require('../models/ProMandats');
 
 /* GET home page. */
-router.get('/promandats', function(req, res, next) {
-  res.render('showPROMANDATS', { title: 'ykola' });
+router.get('/promandats', async function(req, res, next) {
+
+  const promandats = await Promandats.find().sort({
+    id: 'desc'
+  })
+  res.render('showPROMANDATS', { promandats: promandats});
+});
+
+/* GET find by id. */
+router.get('/documents/:id', async (req, res, next) => {
+  const document = await Documents.findById(req.params.id)
+  if (document == null) {
+    res.redirect('/ressources/documents')
+  }else{
+    res.render('detailDOC', {document: document});
+  }  
 });
 
 module.exports = router;
