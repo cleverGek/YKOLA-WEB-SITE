8e7a1300b05b42645cf1473dbb24fe12e92976d9
cleverGek <kg.geku@gmail.com>
2021-04-10 15:54:53 +0100

    routes at views cote client

--- routes/tendances.js
+++ routes/tendances.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var Santes = require('../models/Sante');
+var Cultures = require('../models/Culture');
+var Sports = require('../models/Sport');
+var Politiques = require('../models/Politique');
 
 /* GET SANTE page. */
 router.get('/santes', async function(req, res, next) {
@@ -22,19 +25,63 @@ router.get('/santes/:id', async (req, res, next) => {
 
 /** fin sante */
 
-/* GET POLITIQUE page. */
-router.get('/politique', function(req, res, next) {
-  res.render('showPOLITIQUE', { title: 'ykola' });
+router.get('/politiques', async function(req, res, next) {
+  const Politique = await Politiques.find().sort({
+    id: 'desc'
+  });
+  res.render('showPOLITIQUE', {politiques: Politique});
 });
 
-/* GET SPORT page. */
-router.get('/sport', function(req, res, next) {
-  res.render('showSPORT', { title: 'ykola' });
+/* GET find by id. */
+router.get('/politiques/:id', async (req, res, next) => {
+  const Politique = await Politiques.findById(req.params.id)
+  if (Politique == null) {
+    res.redirect('/politiques')
+  }else{
+    res.render('detailPOLITIQUE', {politique: Politique});
+  }  
 });
 
-/* GET CULTURE page. */
-router.get('/culture', function(req, res, next) {
-  res.render('showCULTURE', { title: 'ykola' });
+/** fin politiques */
+
+router.get('/cultures', async function(req, res, next) {
+  const Culture = await Cultures.find().sort({
+    id: 'desc'
+  });
+  res.render('showCULTURE', {cultures: Culture});
 });
+
+/* GET find by id. */
+router.get('/cultures/:id', async (req, res, next) => {
+  const Culture = await Cultures.findById(req.params.id)
+  if (Culture == null) {
+    res.redirect('/cultures')
+  }else{
+    res.render('detailCULTURE', {cultures: Culture});
+  }  
+});
+
+/** fin culture */
+
+//sport
+
+router.get('/sports', async function(req, res, next) {
+  const Sport = await Sports.find().sort({
+    id: 'desc'
+  });
+  res.render('showSPORT', {sports: Sport});
+});
+
+/* GET find by id. */
+router.get('/sports/:id', async (req, res, next) => {
+  const Sport = await Sports.findById(req.params.id)
+  if (Sport == null) {
+    res.redirect('/sports')
+  }else{
+    res.render('detailSPORT', {sports: Sport});
+  }  
+});
+
+//fin sport
 
 module.exports = router;
