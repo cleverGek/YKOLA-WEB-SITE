222a2c462cb925dbcb3c17adc296b54d593be177
cleverGek <kg.geku@gmail.com>
2021-08-07 12:17:15 +0100

    first commit

--- app.js
+++ app.js
@@ -3,20 +3,11 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+
+require("dotenv").config();
 require("./database/cnx");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var autreRouter = require("./routes/autres");
-var ressourcesRouter = require("./routes/ressources");
-var promandatRouter = require("./routes/proMandats");
-var tendanceRouter = require("./routes/tendances");
-var contactRouter = require("./routes/contact");
-var urlRouter = require("./routes/url");
-var admResRouter = require("./routes/admRessources");
-var admTendances = require("./routes/admTendances");
-var admPromandats = require("./routes/admPromandats");
-var adminRouter = require("./routes/admin");
+//midlleware
 
 var app = express();
 
@@ -30,20 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/", autreRouter);
-app.use("/contact", contactRouter);
-app.use("/url", urlRouter);
-app.use("/ressources/", ressourcesRouter);
-app.use("/", promandatRouter);
-app.use("/tendances", tendanceRouter);
-
-/** admn */
-app.use("/admin", adminRouter);
-app.use("/admin", admResRouter);
-app.use("/admin", admPromandats);
-app.use("/admin", admTendances);
+//root
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
