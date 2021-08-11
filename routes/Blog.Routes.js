23f0f55913d5c19c85d390e3df55d1a3b48f337a
cleverGek <kg.geku@gmail.com>
2021-08-11 09:12:35 +0100

    les pages

--- routes/Blog.Routes.js
+++ routes/Blog.Routes.js
@@ -2,8 +2,11 @@ const express = require("express");
 const { jeux, videos, document } = require("../services/ressources");
 const route = express.Router();
 
+//renders
 route.get("/jeux", jeux);
 route.get("/videos", videos);
 route.get("/documents", document);
+
+//api
 
 module.exports = route;
