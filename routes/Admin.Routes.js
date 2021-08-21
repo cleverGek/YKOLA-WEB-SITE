c81d95e98f73fd3eb163f616f2887d15717736f8
cleverGek <kg.geku@gmail.com>
2021-08-21 20:08:55 +0100

    routes admin

--- routes/Admin.Routes.js
+++ routes/Admin.Routes.js
@@ -1,9 +1,30 @@
 const express = require("express");
-const { admin, categories, articlesPub } = require("../services/render");
+const {
+  admin,
+  categories,
+  articlesPub,
+  secteurs,
+  tendancesPub,
+  check,
+  contact,
+  news,
+} = require("../services/render");
 const route = express.Router();
 
 route.get("/", admin);
+
+/** articles */
 route.get("/categories", categories);
 route.get("/publication_articles", articlesPub);
+
+/** tendance */
+route.get("/secteur", secteurs);
+route.get("/tendancepub", tendancesPub);
+/** verification */
+route.get("/checking", check);
+/** contact */
+route.get("/contactez", contact);
+/** news */
+route.get("/newslleters", news);
 
 module.exports = route;
--- services/render.js
+++ services/render.js
@@ -7,10 +7,32 @@ exports.admin = (req, res) => {
   res.render("utils/admin/pages/home");
 };
 
+/** articles */
+
 exports.categories = (req, res) => {
   res.render("utils/admin/pages/categorie");
 };
 
 exports.articlesPub = (req, res) => {
   res.render("utils/admin/pages/articlepub");
+};
+
+/** tendance */
+exports.secteurs = (req, res) => {
+  res.render("utils/admin/pages/secteur");
+};
+exports.tendancesPub = (req, res) => {
+  res.render("utils/admin/pages/tendancepub");
+};
+/** verification */
+exports.check = (req, res) => {
+  res.render("utils/admin/pages/check");
+};
+/** contact */
+exports.contact = (req, res) => {
+  res.render("utils/admin/pages/contact");
+};
+/** news */
+exports.news = (req, res) => {
+  res.render("utils/admin/pages/news");
 };
